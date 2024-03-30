214d30bc3798fff1b5b46d28d725177064b7e005
Max Zarubitskiy <maka123456789@mail.ru>
2024-03-31 06:52:21 +0700

    change font and work section

--- js/app.js
+++ js/app.js
@@ -32,20 +32,6 @@ function lenis() {
   connectToScrollTrigger();
 }
 
-function pixel() {
-  const pixel = document.querySelectorAll(".pixel__item");
-  var t1 = gsap.timeline();
-  t1.to(pixel, {
-    visibility: "hidden",
-    stagger: {
-      each: 0.05,
-      from: "random",
-    },
-  })
-    .duration(10)
-    .play(1);
-}
-
 async function animDepixelate($el) {
   const $parent = $el.parentNode;
 
@@ -59,53 +45,65 @@ async function animDepixelate($el) {
   canvas.style.zIndex = 10;
   canvas.style.position = "relative";
   $parent.appendChild(canvas);
-  console.log(canvas);
   const ctx = canvas.getContext("2d", { willReadFrequently: true });
-  console.log(ctx);
 
   // handle dimensions
-  // const maxWidth = 280;
+  /// const maxWidth = 280;
   // let w = $el.naturalWidth;
   // let h = $el.naturalHeight;
   // if (w > maxWidth) {
   //   h = maxWidth * (h / w);
   //   w = maxWidth;
   // }
+  // const w = $el.width;
+  // const h = $el.height;
+  // if (w > 120) {
+  //   h = height * (120 / w);
+  //   w = 280;
+  // }
   const w = 280;
   const h = 350;
 
   // do the thing
   const pixelate = async (sample_amount) => {
-      return new Promise(resolve => {
-          if(!canvas.parentNode) {
-              resolve()
-              return
-          }
+    return new Promise((resolve) => {
+      if (!canvas.parentNode) {
+        resolve();
+        return;
+      }
 
-          const sample_size = Math.round(w/sample_amount)
+      const sample_size = Math.round(w / sample_amount);
 
-          ctx.canvas.width = w
-          ctx.canvas.height = h
-          // ctx.drawImage($el, 0, 0);
-          ctx.drawImage($el, 0, 0, w, h);
+      ctx.canvas.width = w;
+      ctx.canvas.height = h;
+      ctx.drawImage($el, 0, 0, w, h);
 
-          const pixelArr = ctx.getImageData(0, 0, w, h).data;
+      const pixelArr = ctx.getImageData(0, 0, w, h).data;
 
-          for (let y = 0; y < h; y += sample_size) {
-              for (let x = 0; x < w; x += sample_size) {
-                  const p = (x + (y*w)) * 4;
-                  ctx.fillStyle = "rgba(" + pixelArr[p] + "," + pixelArr[p + 1] + "," + pixelArr[p + 2] + "," + pixelArr[p + 3] + ")";
-                  ctx.fillRect(x, y, sample_size, sample_size);
-              }
-          }
+      for (let y = 0; y < h; y += sample_size) {
+        for (let x = 0; x < w; x += sample_size) {
+          const p = (x + y * w) * 4;
+          ctx.fillStyle =
+            "rgba(" +
+            pixelArr[p] +
+            "," +
+            pixelArr[p + 1] +
+            "," +
+            pixelArr[p + 2] +
+            "," +
+            pixelArr[p + 3] +
+            ")";
+          ctx.fillRect(x, y, sample_size, sample_size);
+        }
+      }
 
-          resolve()
-      })
-  }
+      resolve();
+    });
+  };
 
   // // timeline
   const ITERATION_DELAY = 1000;
-  const delay = ms => new Promise(res => setTimeout(res, ms));
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
   await pixelate(8);
   await delay(ITERATION_DELAY);
   await pixelate(16);
@@ -123,31 +121,36 @@ async function animDepixelate($el) {
   // await pixelate(512);
   // await delay(ITERATION_DELAY);
 
-  // setTimeout(() => {
-  //   canvas.remove();
-  //   $parent.classList.remove("-pixelated");
-  // }, 1000);
   // remove canvas
   canvas.remove();
-  $parent.classList.remove('-pixelated')
+  $parent.classList.remove("-pixelated");
 }
 
 function hero() {
-  // const hero = document.querySelector(".hero");
-  // const heroTitle = document.querySelector(".hero-title-animation");
-  // const heroTitleWidth = heroTitle.scrollWidth * -2;
-  // // console.log(heroTitle,heroTitleWidth);
-  // gsap.fromTo(
-  //   heroTitle,
-  //   { x: heroTitleWidth },
-  //   {
-  //     x: 0,
-  //     scrollTrigger: {
-  //       trigger: hero,
-  //       scrub: 0.5,
-  //     },
-  //   },
-  // );
+  const canvasHero = document.querySelector(".hero__images__wrapper canvas");
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".hero",
+        start: "top top",
+        end: "bottom top",
+        scrub: !0,
+        markers: !1,
+        onUpdate: (self) => {
+          canvasHero.style.width = `${120 + self.progress * 200}px`;
+          canvasHero.style.height = `${150 + self.progress * 200}px`;
+          if (canvasHero) {
+          }
+        },
+      },
+    })
+    .to(".hero__images__wrapper", {
+      y: "-200",
+      width: "280",
+      height: "350",
+      ease: "linear",
+    });
 }
 
 function intro() {
@@ -184,7 +187,7 @@ function gallery() {
   const galleryItems = document.querySelectorAll(".gallery__item");
   const galleryItemsInner = document.querySelectorAll(".gallery__item-inner");
   const gallery_x1 = [650, -350, -300, 300, 0];
-  const gallery_y1 = [100, 300, 200, 300, 0];
+  const gallery_y1 = [100, 300, 200, 300, -5];
   const gallery_z1 = [
     -1400.35064, -2159.08714, -2375.71775, -2915.29581, -3414.68899,
   ];
@@ -238,13 +241,162 @@ function gallery() {
         scale: 0.9,
       },
       {
-        scale: 0.21,
+        scale: 0.35,
       },
       0,
     );
 }
 
-function works() {}
+function works() {
+  const galleryItemsInner = document.querySelectorAll(".gallery__item-inner");
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".works",
+        start: "top top",
+        end: "50px top",
+        scrub: !0,
+        markers: !1,
+      },
+    })
+    .to(galleryItemsInner[4], {
+      opacity: 0,
+    })
+    // .fromTo(".works__item", { opacity: 0 }, { opacity: 1 }),
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#works__img1",
+          start: "bottom 80%",
+          end: "top 0",
+          scrub: !0,
+          markers: !1,
+        },
+      })
+      .to("#works__img1", {
+        "--scale": 1,
+        "--ty": 0,
+        ease: "linear",
+      }),
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: ".works",
+          start: "top top",
+          end: "400px top",
+          scrub: !0,
+          markers: !1,
+        },
+      })
+      
+      .to("#works__item1", {
+        "--ty": "400px",
+        ease: "linear",
+      }),
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#works__item2",
+          start: "top bottom",
+          end: "bottom bottom",
+          scrub: !0,
+          markers: !1,
+        },
+      })
+      .to("#works__item1", {
+        "--scale": 0.8,
+        "--ty2": "100%",
+        ease: "linear",
+      }),
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#works__item2",
+          start: "bottom bottom",
+          end: "+=400px",
+          scrub: !0,
+          markers: !1,
+        },
+      })
+      .to("#works__item2", {
+        "--ty": "400px",
+        ease: "linear",
+      }),
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#works__item3",
+          start: "top bottom",
+          end: "bottom bottom",
+          scrub: !0,
+          markers: !1,
+        },
+      })
+      .to("#works__item2", {
+        "--scale": 0.8,
+        "--ty2": "100%",
+        ease: "linear",
+      }),
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#works__item3",
+          start: "bottom bottom",
+          end: "+=400px",
+          scrub: !0,
+          markers: !1,
+        },
+      })
+      .to("#works__item3", {
+        "--ty": "400px",
+        ease: "linear",
+      }),
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#works__item4",
+          start: "top bottom",
+          end: "bottom bottom",
+          scrub: !0,
+          markers: !1,
+        },
+      })
+      .to("#works__item3", {
+        "--scale": 0.8,
+        "--ty2": "100%",
+        ease: "linear",
+      }),
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#works__item4",
+          start: "bottom bottom",
+          end: "+=400px",
+          scrub: !0,
+          markers: !1,
+        },
+      })
+      .to("#works__item4", {
+        "--ty": "400px",
+        ease: "linear",
+      }),
+    gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: "#works__item5",
+          start: "top bottom",
+          end: "bottom bottom",
+          scrub: !0,
+          markers: !1,
+        },
+      })
+      .to("#works__item4", {
+        "--scale": 0.8,
+        "--ty2": "100%",
+        ease: "linear",
+      });
+}
 
 function brands() {
   const brands = document.querySelector(".brands");
@@ -292,15 +444,38 @@ function handleFooterCursor() {
 
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
-  
-  window.scrollTo({ top: 0, behavior: 'smooth' });
+
+  window.scrollTo({ top: 0, behavior: "smooth" });
 
   lenis();
+
+  const heroImages = [
+    "images/dist/hero_1.jpg",
+    "images/dist/hero_2.jpg",
+    "images/dist/hero_3.jpg",
+  ];
+  let currentImage = 1;
+
   const element = document.querySelector(".hero__images");
-  // console.log(element);
-  setTimeout(() => {
-      animDepixelate(element);
-    }, 500);
+  if (element) {
+    animDepixelate(element);
+
+    setInterval(() => {
+      if (currentImage >= heroImages.length) {
+        currentImage = 0;
+      }
+
+      gsap.set(element, {
+        attr: { src: heroImages[currentImage] },
+        onbeforeunload: () => {
+          animDepixelate(element);
+        },
+      });
+
+      currentImage++;
+    }, 5000);
+  }
+
   // clearInterval(time);
   // Hero title animation
   // pixel();
